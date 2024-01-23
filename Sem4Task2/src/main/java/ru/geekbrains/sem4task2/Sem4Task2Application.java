6289f4565f30d19edb6483831ec56225468c974a
Artem Maklashev <artem@maklashev.ru>
2024-01-23 15:56:44 +0400

    HW4 task2 init

---
+++ Sem4Task2/src/main/java/ru/geekbrains/sem4task2/Sem4Task2Application.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.sem4task2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sem4Task2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sem4Task2Application.class, args);
+    }
+
+}
---
+++ Sem4Task2/src/test/java/ru/geekbrains/sem4task2/Sem4Task2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.sem4task2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sem4Task2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
