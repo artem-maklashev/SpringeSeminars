6b4e40cf08f7d6ba191b7d4c800c737f3788d944
Artem Maklashev <artem@maklashev.ru>
2024-01-25 12:59:42 +0400

    HW5 task2 init

---
+++ sem5task2/src/test/java/ru/geekbrains/sem5task2/Sem5task2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.sem5task2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sem5task2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
