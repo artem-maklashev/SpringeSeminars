aa1a26cd69ddafbc50783978dfb2cbe372c0ce44
Maklashev-Artem <artem@maklashev.ru>
2024-01-27 10:11:08 +0400

    db init add properties to application.properties

---
+++ sem6task1/src/main/java/gb/sem6task1/Sem6task1Application.java
@@ -0,0 +1,13 @@
+package gb.sem6task1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sem6task1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sem6task1Application.class, args);
+	}
+
+}
---
+++ sem6task1/src/test/java/gb/sem6task1/Sem6task1ApplicationTests.java
@@ -0,0 +1,13 @@
+package gb.sem6task1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sem6task1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
