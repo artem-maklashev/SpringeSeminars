44a82a820942cc4cb647b10d72fbff998057550f
Маклашев Артем <maklashev_ak@outlook.com>
2024-01-29 21:06:26 +0400

    HW6 task2 done убрал лишние пакеты

--- sem5/target/classes/schema.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS userTable (
-     id BIGINT AUTO_INCREMENT PRIMARY KEY,
-     description varchar(150) NOT NULL,
-     task_status varchar(10) NOT NULL,
-     creation_date DATETIME
-);
