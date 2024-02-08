04921482111eb8e03ad7005cdadab72c5c467cf7
Artem Maklashev <artem@maklashev.ru>
2024-02-08 16:17:54 +0400

    HW8 in progress

---
+++ apiGateway/src/test/java/ru/geekbrains/apiGateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.apiGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiFatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
